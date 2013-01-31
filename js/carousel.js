416b6eb6f47621855cd74bce617387e7b670eff3
Daniel Husar <dano.husar@gmail.com>
2013-01-31 16:53:45 +0100

    main logic

--- js/carousel.js
+++ js/carousel.js
@@ -9,38 +9,107 @@
 	$.fn.extend({
 		carousel: function (options) {
 
+			//settings
 			var settings = $.extend( {
-				'start'   : 1              //starting slide,
-				'easing'  : 'easeOutExpo'  //easing type
+				'arrows'     : true,           //display navigation arrows
+				'navigation' : true,           //display navigation
+				'pause'      : 5000,           //pause between slides
+				'speed'      : 2000,           //speed for animation
+				'easing'     : 'easeOutExpo'   //easing type
 			}, options); 
+
+			//helpers
+			var helpers = {
+				css3transformations : function () {
+		    	var $elem = $('<div />').css({
+		        'webkitTransform': 'matrix(1,1,1,1,1,1)',
+		        'MozTransform': 'matrix(1,1,1,1,1,1)',
+		        'transform': 'matrix(1,1,1,1,1,1)'
+		      });
+		      
+		      if($elem.attr('style') === '') {
+		        return false;
+		      } else if($elem.attr('style') !== undefined) {
+		        return true;
+		      }
+		      return false;
+				}
+			};
+
+			//main interval
+			var interval;
 
 
 			return $(this).each(function(){
-				var $that = $(this);
+				var $carousel = $(this);
 
 				//init carousel after frist image is loaded
-				$that.find('li > img').first().load(function(){
-					var $li    = $that.find('li').first(),
-							$img   = $li.find('img').first(),
-							width  = $img.width(),
-							height = $img.height(),
-							count  = $img.length;
+				$carousel.find('li > img').first().load(function(){
 
-					$that.wrap('<div class="carousel"/>').wrap('<div class="carousel-content" />');
+					var $li    				= $carousel.find('li'),
+							$img   				= $carousel.find('img').first(),
+							width  				= $img.width(),
+							height 				= $img.height(),
+							cssTransforms = helpers.css3transformations(),
+							count         = $li.length;
 
+					//wrappers
+					$carousel.wrap('<div class="carousel"/>').wrap('<div class="carousel-content" />');
+
+					//basic styles
 					$('.carousel-content, .carousel').css({
 						'width'  : width + 'px',
 						'height' : height + 'px'
 					});
 
 					$('.carousel-content ul').css({
-						'width'  : count * width + 'px',
-						'height' : height + 'px'
+						'width'  							: count * width + 'px',
+						'height' 							: height + 'px'
 					});
 
-					$('.carousel').prepend('<a href="#" class="arrow next">></a>');
-					$('.carousel').prepend('<a href="#" class="arrow prev"><</a>');
-					$('.carousel').prepend('<div class="navigation"><a href="#" class="current"></a><a href="#"></a><a href="#"></a></div>');
+					if(cssTransforms){
+						$('.carousel-content ul').css({
+						'-webkit-transition'	: settings.speed/1000 + 's ease-in-out',
+  					'-moz-transition' 		: settings.speed/1000 + 's ease-in-out',
+  				  '-o-transition' 			: settings.speed/1000 + 's ease-in-out',
+  					'transition' 					: settings.speed/1000 + 's ease-in-out'
+					});
+					}
+
+					//arrows
+					if(settings.arrows){
+						$('.carousel').prepend('<a href="#" class="arrow next">></a>');
+						$('.carousel').prepend('<a href="#" class="arrow prev"><</a>');
+					}
+
+					//navigation
+					if(settings.navigation){
+						var navItems = '',
+								navClass;
+						for(var i=1; i<=count; i++){
+							navClass = ( i === 1) ? 'current' : '';
+							navItems = navItems + '<a href="#' + i + '" class="' + navClass + '"></a>';
+						};
+						$('.carousel').prepend('<div class="navigation">' + navItems + '</div>');
+					}
+
+					//slide content
+					function slideContent(){
+						var currentPos = -parseInt(($carousel.css('left')).replace('px', ''))/width || 0,
+						    nextPos    = (currentPos < count - 1) ? currentPos + 1 : 1,
+								position   = -nextPos * width;
+
+						if(cssTransforms){
+							$carousel.css({'left': position + 'px'});
+						}else{
+							$carousel.animate({'left': position + 'px'}, settings.speed);
+						}
+					}
+
+					//run the interval
+					setInterval(function(){
+						slideContent();
+					}, settings.pause);
 
 				});
 
